4003f54938c75129d5697cacec74f81190587c07
Scoheart <Scoheart1487790559@gmail.com>
2023-02-25 20:28:43 +0800

    @scoheart/vue launched

--- packages/scoheart-demo/index.js
+++ packages/scoheart-demo/index.js
@@ -1,8 +1,35 @@
+import Vue from "@scoheart/vue"
 import axios from '@scoheart/axios'
+
+let vm = new Vue({
+    el: "#app",
+    data: {
+        basic: {
+            name: "shuhao",
+            age: 12,
+            gender: "male",
+            addr: "",
+            email: "s",
+            tel: "ds"
+        },
+        education: {
+            school: "sd"
+        }
+    }
+})
+
+console.log(vm)
+// vm.basic.name = {
+//     name: "shuhao",
+//     age: 23
+// }
+
+
 
 axios({
     method: "get",
-    url: "http://localhost/get"
-}).then(v => {
-    console.log(v)
+    url: "http://localhost:8080/get"
+}).then(res => {
+    vm.basic = res
 })
+
